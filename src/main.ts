d1aa9a16c37052989e2ffb1957e8e91fef68c38c
LunXr <178067403+LunXrDev@users.noreply.github.com>
2024-08-11 18:53:11 +0800

    Moved server listen to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 import { CozyCopClient } from "./structures/Client"
+import http from "http"
 
 const Client = new CozyCopClient();
 
 Client.startClient();
 
-console.log(process.env.TOKEN);
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`
+      <html>
+        <head>
+          <title>Your Web View</title>
+        </head>
+        <body style="margin: 0; padding: 0;">
+          <iframe width="100%" height="100%" src="https://axocoder.vercel.app/" frameborder="0" allowfullscreen></iframe>
+        </body>
+      </html>`);
+  });
+
+  server.listen(3000);
--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -2,20 +2,6 @@ import { Client, IntentsBitField, Partials } from "discord.js";
 import http from "http";
 
 const dotenv = require("dotenv").config();
-
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`
-      <html>
-        <head>
-          <title>Your Web View</title>
-        </head>
-        <body style="margin: 0; padding: 0;">
-          <iframe width="100%" height="100%" src="https://axocoder.vercel.app/" frameborder="0" allowfullscreen></iframe>
-        </body>
-      </html>`);
-  });
-
 export class CozyCopClient extends Client {
     constructor() {
         super({
@@ -50,9 +36,5 @@ export class CozyCopClient extends Client {
 
     public async startClient() {
         super.login(process.env.TOKEN);
-
-        server.listen(3000, () => {
-            console.log('Server Online because of Axo Coder ✅!!');
-          });
     }
 }
